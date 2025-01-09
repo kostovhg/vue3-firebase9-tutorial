4752e0890854396021a746abbbb307e6c796ebb6
Hristo Kostov <kostovhg@gmail.com>
2025-01-09 16:21:27 +0200

    - Primitive navbar and notworking logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,29 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import { useTaskSnapStore } from "@/stores/useTaskSnapStore";
+import { useOpsStore } from "@/stores/useOpsStore";
+
+const theStore = useTaskSnapStore();
+const opsStore = useOpsStore();
+</script>
 
 <template>
   <div class="navbar-menu">
     <!-- navbar items -->
     <div class="navbar-start">
-      <image src="./assets/logo-mat.png" />
+      <div class="navbar-item">
+        <image :src="'@/assets/logo.png'" />
+      </div>
+      <div class="navbar-item">
+        ПРОЕКТИ за {{ opsStore.operations[theStore.operation].bgName }}
+      </div>
     </div>
-    <div class="navbar-item">link 2</div>
-  </div>
 
-  <div class="navbar-end">
-    <!-- navbar items -->
-    <div class="navbar-item">Link 3</div>
-    <div class="navbar-item">link 4</div>
-    <div class="navbar-item">link 5</div>
+    <div class="navbar-end">
+      <!-- navbar items -->
+      <div class="navbar-item">Link 3</div>
+      <div class="navbar-item">link 4</div>
+      <div class="navbar-item">link 5</div>
+    </div>
   </div>
 </template>
